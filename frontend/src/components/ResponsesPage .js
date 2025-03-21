a8990e4e38827e47b872a9341d20b4ca86a73b9f
mortadhaksontini <mortadhaksontini22@gmail.com>
2025-03-21 09:24:07 +0000

    adding view responses for a form

--- frontend/src/components/ResponsesPage .js
+++ frontend/src/components/ResponsesPage .js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { ClipLoader } from "react-spinners";
 
 const ResponsesPage = () => {
   const { formId } = useParams();
