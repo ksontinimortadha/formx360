932238dc5c34bd67d0f3f372f674c6ab513b4f97
mortadhaksontini <mortadhaksontini22@gmail.com>
2025-04-10 22:15:46 +0100

    adding real time notification

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // CORS configuration
 const corsOptions = {
   origin: "http://localhost:3000", // Allow only your frontend
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST","DELETE","PUT"],
   credentials: true, // Allow cookies and other credentials
 };
 
@@ -33,7 +33,7 @@ const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: "http://localhost:3000", // Allow only your frontend
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   },
 });
